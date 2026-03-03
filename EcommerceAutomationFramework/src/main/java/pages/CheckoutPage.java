d016f796e8bad21106deb081046ea79976ed52be
Sangavk18 <108139281+Sangavk18@users.noreply.github.com>
2026-03-03 19:17:11 +0530

    Update CheckoutPage.java

--- EcommerceAutomationFramework/src/main/java/pages/CheckoutPage.java
+++ EcommerceAutomationFramework/src/main/java/pages/CheckoutPage.java
@@ -10,47 +10,42 @@ public class CheckoutPage {
     WebDriver driver;
     WebDriverWait wait;
 
-    @FindBy(id="first-name")
+    @FindBy(id = "first-name")
     WebElement firstName;
 
-    @FindBy(id="last-name")
+    @FindBy(id = "last-name")
     WebElement lastName;
 
-    @FindBy(id="postal-code")
+    @FindBy(id = "postal-code")
     WebElement postalCode;
 
-    @FindBy(id="continue")
+    @FindBy(id = "continue")
     WebElement continueBtn;
 
-    @FindBy(id="finish")
+    @FindBy(id = "finish")
     WebElement finishBtn;
 
     public CheckoutPage(WebDriver driver) {
         this.driver = driver;
-        this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(15));
         PageFactory.initElements(driver, this);
     }
 
     public CheckoutPage enterDetails() {
 
-        wait.until(ExpectedConditions.visibilityOf(firstName))
-                .sendKeys("John");
+        wait.until(ExpectedConditions.visibilityOf(firstName)).sendKeys("John");
+        lastName.sendKeys("Doe");
+        postalCode.sendKeys("400001");
 
-        wait.until(ExpectedConditions.visibilityOf(lastName))
-                .sendKeys("Doe");
-
-        wait.until(ExpectedConditions.visibilityOf(postalCode))
-                .sendKeys("400001");
-
-        wait.until(ExpectedConditions.elementToBeClickable(continueBtn))
-                .click();
+        wait.until(ExpectedConditions.elementToBeClickable(continueBtn)).click();
 
         return this;
     }
 
     public ConfirmationPage finishOrder() {
-        wait.until(ExpectedConditions.elementToBeClickable(finishBtn))
-                .click();
+
+        wait.until(ExpectedConditions.elementToBeClickable(finishBtn)).click();
+
         return new ConfirmationPage(driver);
     }
 }
